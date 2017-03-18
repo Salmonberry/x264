7f69e5b42dbdcb40ece7467a70cf595b509de965
Henrik Gramner <henrik@gramner.com>
2017-03-18 18:50:36 +0100

    x86: Change assembler from yasm to nasm
    This is required to support AVX-512.

--- common/x86/x86inc.asm
+++ common/x86/x86inc.asm
@@ -1528,16 +1528,3 @@ FMA4_INSTR fmsub,    pd, ps, sd, ss
 FMA4_INSTR fmsubadd, pd, ps
 FMA4_INSTR fnmadd,   pd, ps, sd, ss
 FMA4_INSTR fnmsub,   pd, ps, sd, ss
-
-; workaround: vpbroadcastq is broken in x86_32 due to a yasm bug (fixed in 1.3.0)
-%ifdef __YASM_VER__
-    %if __YASM_VERSION_ID__ < 0x01030000 && ARCH_X86_64 == 0
-        %macro vpbroadcastq 2
-            %if sizeof%1 == 16
-                movddup %1, %2
-            %else
-                vbroadcastsd %1, %2
-            %endif
-        %endmacro
-    %endif
-%endif
