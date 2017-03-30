jeeb/x264_L-SMASH

Detect color matrix with lavf/ffms demuxer

input/ffms.c
@@ -141,6 +141,12 @@ static int open_file( char *psz_filename, hnd_t *p_handle, video_info_t *info, c
     /* ffms is thread unsafe as it uses a single frame buffer for all frame requests */
     info->thread_safe  = 0;
 
+    /* -1 = 'unset' (internal) , 2 from lavf|ffms = 'unset' */
+    if( videop->ColorSpace >= 0 && videop->ColorSpace <= 8 && videop->ColorSpace != 2 )
+        info->colormatrix = videop->ColorSpace;
+    else
+        info->colormatrix = -1;
+
     const FFMS_Frame *frame = FFMS_GetFrame( h->video_source, 0, &e );
     FAIL_IF_ERROR( !frame, "could not read frame 0\n" )
 

Salmonberry/x264

input/ffms.c
@@ -137,6 +137,12 @@ static int open_file( char *psz_filename, hnd_t *p_handle, video_info_t *info, c
     const FFMS_Frame *frame = FFMS_GetFrame( h->video_source, 0, &e );
     FAIL_IF_ERROR( !frame, "could not read frame 0\n" );
 
+    /* -1 = 'unset' (internal) , 2 from lavf|ffms = 'unset' */
+    if( frame->ColorSpace >= 0 && frame->ColorSpace <= 8 && frame->ColorSpace != 2 )
+        info->colormatrix = frame->ColorSpace;
+    else
+        info->colormatrix = -1;
+
     info->fullrange  = 0;
     info->width      = frame->EncodedWidth;
     info->height     = frame->EncodedHeight;